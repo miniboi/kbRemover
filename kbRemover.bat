0ab38d62faa533e76823fa55465d452e747b373c
Kusberg <kusberg@github.com>
2017-12-10 15:02:30 +0100

    First batch-file update

---
+++ kbRemover.bat
@@ -0,0 +1,9 @@
+@echo off
+wusa /uninstall /kb:3068708 /norestart
+wusa /uninstall /kb:3022345 /norestart
+wusa /uninstall /kb:3075249 /norestart
+wusa /uninstall /kb:3080149 /norestart
+wusa /uninstall /kb:3035583 /norestart
+wusa /uninstall /kb:3068708 /norestart
+wusa /uninstall /kb:3075249 /norestart
+wusa /uninstall /kb:3080149 /norestart
